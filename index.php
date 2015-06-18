a7069a9cdadae10c05c4819eb3798d31f8a8f146
JaeHo Song <thruthesky@gmail.com>
2015-06-18 14:29:54 +0900

    work updat

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-	if ( isset($_GET['action']) ) $action = $_GET['action'];
+	if ( isset($_GET['action']) && $_GET['action'] ) $action = $_GET['action'];
 	else $action = 'form';
 ?>
 <!doctype html>
